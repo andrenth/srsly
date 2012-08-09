dbaf1cd00add22df0acebc050d8fd1455edbf59c
Andre Nathan <andre@digirati.com.br>
2012-08-09 10:18:39 -0300

    Remove unused file

--- src/postfix.mli
+++
@@ -1,8 +0,0 @@
-type attrs
-
-val instance : attrs -> string
-val client_address : attrs -> string
-val helo_name : attrs -> string
-val sender : attrs -> string
-
-val parse_attrs : Lwt_unix.file_descr -> attrs option Lwt.t
