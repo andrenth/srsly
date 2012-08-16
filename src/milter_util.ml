6e283bf8a1a54aafbd0df604710208e654a3634d
Andre Nathan <andre@digirati.com.br>
2012-08-16 16:29:49 -0300

    Removing obsolete code

--- src/milter_util.ml
+++ src/milter_util.ml
@@ -1,6 +1,7 @@
 open Lwt
 open Printf
 open Ipc.Slave_types
+open Log
 open Util
 
 module O = Release_option
@@ -27,29 +28,6 @@ module StepSet = SetOfList.Make(struct
   type t = Milter.step
   let compare = compare
 end)
-
-let syslog = ref None
-
-let open_log () =
-  let myname = Filename.basename Sys.argv.(0) in
-  let log = Syslog.openlog ~facility:`LOG_DAEMON ~flags:[`LOG_PID] myname in
-  syslog := Some log
-
-let log level fmt =
-  ksprintf (fun s -> Syslog.syslog (O.some !syslog) level (s^"\n")) fmt
-
-let close_log () =
-  O.may Syslog.closelog !syslog
-
-let setup_syslog () =
-  open_log ();
-  Lwt_main.at_exit (fun () -> close_log (); return ())
-
-let debug fmt = log `LOG_DEBUG fmt
-let notice fmt = log `LOG_NOTICE fmt
-let info fmt = log `LOG_INFO fmt
-let warning fmt = log `LOG_WARNING fmt
-let error fmt = log `LOG_ERR fmt
 
 let with_priv_data z ctx f =
   match Milter.getpriv ctx with
