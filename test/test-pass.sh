0e85156c69b1683238f75ca0c6b32f716a9781e1
Andre Nathan <andre@digirati.com.br>
2012-08-29 11:44:57 -0300

    Add some test parameters

--- test/test-pass.sh
+++ test/test-pass.sh
@@ -2,15 +2,21 @@
 
 . `dirname $0`/config.sh
 
-ENVFROM=spf-test@digirati.com.br
-HELOADDR="`host mxz.f1.k8.com.br | head -n1 | awk '{ print $NF}'`"
+ENVFROM=andrenth@gmail.com
+ENVRCPT1=andrenth@gmail.com
+ENVRCPT2=andrenth@digirati.com.br
+HELOADDR="`host mail-we0-f179.google.com | head -n1 | awk '{ print $NF}'`"
 HELOFQDN="`host $HELOADDR | head -n1 | awk '{ print $NF}'`"
 
-runtest "`milter-test-server                      \
-            --connection-spec $CONNSPEC           \
-            --connect-address inet:9999@$HELOADDR \
-            --helo-fqdn       $HELOFQDN           \
-            --envelope-from   $ENVFROM`"          \
+runtest "`milter-test-server                         \
+            --connection-spec    $CONNSPEC           \
+            --connect-address    inet:9999@$HELOADDR \
+            --connect-host       $HELOFQDN           \
+            --helo-fqdn          $HELOFQDN           \
+            --envelope-recipient $ENVRCPT1           \
+            --envelope-recipient $ENVRCPT2           \
+            --threads=0                              \
+            --envelope-from   $ENVFROM`"             \
         '^status: pass'
 
 exit $?
